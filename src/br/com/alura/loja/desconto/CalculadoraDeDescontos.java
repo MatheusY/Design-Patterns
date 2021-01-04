cf2bd563b515876f37aeb3e988ee0f0c4ce7e424
matheus yudi <matheus.matsubara12@gmail.com>
2021-01-04 18:42:11 -0300

    Template Method

--- src/br/com/alura/loja/desconto/CalculadoraDeDescontos.java
+++ src/br/com/alura/loja/desconto/CalculadoraDeDescontos.java
@@ -7,10 +7,10 @@ import br.com.alura.loja.orcamento.Orcamento;
 public class CalculadoraDeDescontos {
 
 	public BigDecimal calcular(Orcamento orcamento) {
-		Desconto desconto = new DescontoParaPagamentosComMaisDeCincoItens(
+		Desconto cadeiaDesconto = new DescontoParaPagamentosComMaisDeCincoItens(
 				new DescontoParaPagamentosComValorMaiorDoQueQuinhentos(
 						new SemDesconto()));
 		
-		return desconto.calcular(orcamento);
+		return cadeiaDesconto.calcular(orcamento);
 	}
 }
