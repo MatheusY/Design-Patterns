eba3e43438fd934a55063770e46e760d64c017c3
matheus yudi <matheus.matsubara12@gmail.com>
2021-01-07 17:15:29 -0300

    Decorator

--- src/br/com/alura/loja/imposto/Imposto.java
+++ src/br/com/alura/loja/imposto/Imposto.java
@@ -1,9 +1,28 @@
 package br.com.alura.loja.imposto;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 import br.com.alura.loja.orcamento.Orcamento;
 
-public interface Imposto {
-	BigDecimal calcular(Orcamento orcamento);
+public abstract class Imposto {
+	
+	private Imposto outro;
+	
+	
+	public Imposto(Imposto outro) {
+		this.outro = outro;
+	}
+
+	protected abstract BigDecimal realizarCalculo(Orcamento orcamento);
+	
+	public BigDecimal calcular(Orcamento orcamento) {
+		BigDecimal valorImposto = realizarCalculo(orcamento);
+		BigDecimal valorDoOutroImposto = BigDecimal.ZERO;
+		if(Objects.nonNull(outro)) {
+			valorDoOutroImposto = outro.realizarCalculo(orcamento);
+		}
+		
+		return valorImposto.add(valorDoOutroImposto);
+	}
 }
