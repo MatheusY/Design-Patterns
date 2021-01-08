871f398a013e55627a8147f8f8730d713e2a44e8
matheus yudi <matheus.matsubara12@gmail.com>
2021-01-08 16:06:32 -0300

    Facade

--- src/br/com/alura/loja/pedido/GeraPedidoHandler.java
+++ src/br/com/alura/loja/pedido/GeraPedidoHandler.java
@@ -1,25 +1,27 @@
 package br.com.alura.loja.pedido;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import br.com.alura.loja.orcamento.ItemOrcamento;
 import br.com.alura.loja.orcamento.Orcamento;
 import br.com.alura.loja.pedido.acao.AcaoAposGeraPedido;
 
 public class GeraPedidoHandler {
-	
+
 	List<AcaoAposGeraPedido> acoes;
-	
+
 	public GeraPedidoHandler(List<AcaoAposGeraPedido> acoes) {
 		this.acoes = acoes;
 	}
-	//Injeção de dependencias
-	
+	// Injeção de dependencias
+
 	public void execute(GeraPedido dados) {
-	Orcamento orcamento = new Orcamento(dados.getValorOrcamento(), dados.getQuantidadeItens());
-		
+		Orcamento orcamento = new Orcamento();
+		orcamento.adicionarItem(new ItemOrcamento(new BigDecimal("200")));
 		Pedido pedido = new Pedido(dados.getCliente(), LocalDateTime.now(), orcamento);
-		
+
 		acoes.forEach(a -> a.executarAcao(pedido));
 	}
 }
